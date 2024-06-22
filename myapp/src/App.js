9be2c04dc7213325aedb39fc66052e3d0258c9f0
Deepak GS <deepakgs.4737@gmail.com>
2024-06-22 07:57:07 +0530

    first commit

---
+++ myapp/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import { useState } from 'react';
+import React, { Fragment } from 'react';
+
+
+function App() {
+  
+  return(
+    <he>Hello world!</he>
+  );
+}
+
+export default App;
